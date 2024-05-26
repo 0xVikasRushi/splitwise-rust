01ddc7dd0155e100a14a01d11eb9e7dafeaa414b
0xvikasrushi <0xvikas@gmail.com>
2024-05-26 13:02:34 +0530

    feat: split fn

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,7 @@ struct Transactions {
     transactions: Vec<Transaction>,
 }
 
+#[derive(Clone)]
 struct Net {
     user: User,
     amount: i32,
@@ -40,26 +41,27 @@ impl Transactions {
             transactions: Vec::new(),
         }
     }
+
     fn add(&mut self, tx: Transaction) {
         self.transactions.push(tx);
     }
 
-    fn display(&mut self) {
+    fn display(&self) {
         for i in &self.transactions {
             println!(
-                "------> from : {} ,to : {}, amount ,{}",
-                i.from.name, i.to.name, i.amount
+                "------> {} pays : {}  amount to {}",
+                i.from.name, i.amount, i.to.name
             );
         }
     }
 
-    fn calc_net(&mut self) -> (Vec<Net>, Vec<Net>) {
+    fn calc_net(&self) -> (Vec<Net>, Vec<Net>) {
         let mut map: HashMap<String, i32> = HashMap::new();
 
         for i in &self.transactions {
             let from = i.from.name.clone();
             let to = i.to.name.clone();
-            let amount = i.amount.clone() as i32;
+            let amount = i.amount as i32;
 
             *map.entry(from.clone()).or_insert(0) -= amount;
             *map.entry(to.clone()).or_insert(0) += amount;
@@ -81,42 +83,62 @@ impl Transactions {
                 });
             }
         }
-        return (net_positive, net_negative);
+        (net_positive, net_negative)
     }
 
-    // fn split(positive: Vec<Net>, negative: Vec<Net>) -> Transactions {
-    //     let answer = Transactions::new();
-    //     answer
-    // }
+    fn split(positive: &mut Vec<Net>, negative: &mut Vec<Net>) -> Transactions {
+        let mut answer = Transactions::new();
+
+        while !positive.is_empty() && !negative.is_empty() {
+            let mut pos = positive.pop().unwrap();
+            let mut neg = negative.pop().unwrap();
+
+            let settle_amount = std::cmp::min(pos.amount, -neg.amount);
+
+            pos.amount -= settle_amount;
+            neg.amount += settle_amount;
+
+            answer.add(Transaction::new(
+                neg.user.clone(),
+                pos.user.clone(),
+                settle_amount as u64,
+            ));
+
+            if pos.amount > 0 {
+                positive.push(pos);
+            }
+            if neg.amount < 0 {
+                negative.push(neg);
+            }
+        }
+
+        answer
+    }
 }
 
 fn main() {
     let user1 = User::create_user("Alice");
     let user2 = User::create_user("Bob");
     let user3 = User::create_user("Charlie");
-    let user4 = User::create_user("Dave");
 
     let mut all_transactions = Transactions::new();
 
     let tx1 = Transaction::new(user1.clone(), user2.clone(), 10);
-    let tx2 = Transaction::new(user2.clone(), user3.clone(), 20);
-    let tx3 = Transaction::new(user3.clone(), user4.clone(), 30);
-    let tx4 = Transaction::new(user1.clone(), user2.clone(), 40);
+    let tx2 = Transaction::new(user2.clone(), user1.clone(), 1);
+    let tx3 = Transaction::new(user2.clone(), user3.clone(), 5);
+    let tx4 = Transaction::new(user3.clone(), user1.clone(), 5);
 
     all_transactions.add(tx1);
     all_transactions.add(tx2);
     all_transactions.add(tx3);
     all_transactions.add(tx4);
-
+    println!();
     all_transactions.display();
-    let (pos, neg) = all_transactions.calc_net();
+    println!("< --------------------SPLIT BILL OUTPUT -------------------------->");
+    println!();
 
-    println!("---------------------------");
-    for i in pos {
-        println!(" {} : {}", i.user.name, i.amount);
-    }
+    let (mut pos, mut neg) = all_transactions.calc_net();
 
-    for i in neg {
-        println!(" {} : {}", i.user.name, i.amount);
-    }
+    let settled_transactions = Transactions::split(&mut pos, &mut neg);
+    settled_transactions.display();
 }
