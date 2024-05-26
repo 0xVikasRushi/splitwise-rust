f3d79185978f9ab7b5fd7243491feb5b66f2aab3
0xvikasrushi <0xvikas@gmail.com>
2024-05-26 15:43:09 +0530

    feat: cli options

--- README.md
+++ README.md
@@ -5,5 +5,4 @@
   - Create Users back to origin Screen
 - 2. Add Expense
   - paid by amount
-  - parse like VIKAS ALICE 90
 - 3 Bill SplitWise
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,50 @@
-mod split;
-use split::*;
+use std::process::exit;
 
-fn main() {}
+use inquire::{InquireError, Select};
+
+fn main() {
+    loop {
+        let options: Vec<&str> = vec![
+            "1. Create Group",
+            "2. Add Expense",
+            "3. Split Bill",
+            "4. Exit",
+        ];
+
+        let selected_prompt: Result<&str, InquireError> =
+            Select::new("Please Select one Option", options).prompt();
+
+        match selected_prompt {
+            Ok(choice) => {
+                handle_choice(choice);
+            }
+            Err(_) => println!("There was an error, please try again"),
+        }
+    }
+}
+
+fn handle_choice(choice: &str) {
+    match choice {
+        "1. Create Group" => create_group(),
+        "2. Add Expense" => add_expense(),
+        "3. Split Bill" => split_bill(),
+        "4. Exit" => handle_exit(),
+        _ => println!("Invalid choice!"),
+    }
+}
+
+fn create_group() {
+    println!("Creating Group...");
+}
+
+fn add_expense() {
+    println!("Adding Expense...");
+}
+
+fn split_bill() {
+    println!("Splitting Bill...");
+}
+fn handle_exit() {
+    println!("Exiting CLI SUI");
+    exit(0);
+}
